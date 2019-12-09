bddc89be3e1d510de446eb76dc4f4b897629c6f0
Jody Hoon-Starr <jodyhoonstarr@gmail.com>
2019-12-09 17:08:20 -0500

    create occupation landing page with routes

--- src/views/Occupation.vue
+++ src/views/Occupation.vue
@@ -1,16 +1,27 @@
 <template>
-  <div>
+  <v-container>
     <h1 class="text-center">This is the Occupation page</h1>
-    <DataLayout />
-  </div>
+    <v-row>
+      <v-col cols="12" xs="12" sm="6">
+        <v-btn block outlined color="secondary" to="/detailedoccupation">Detailed Occupation</v-btn>
+      </v-col>
+      <v-col cols="12" xs="12" sm="6">
+        <v-btn block outlined color="secondary" to="/occupationbypaygrade">Occupation by Paygrade</v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" xs="12" sm="6">
+        <v-btn block outlined color="secondary" to="/occupationbysector">Occupation by Sector</v-btn>
+      </v-col>
+      <v-col cols="12" xs="12" sm="6">
+        <v-btn block outlined color="secondary" to="/occupationbystate">Occupation by State</v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-import DataLayout from "@/components/DataLayout.vue";
 export default {
-  name: "Occupation",
-  components: {
-    DataLayout
-  }
+  name: "Occupation"
 };
 </script>
