5e7e50db8a00e084ed7267d3bb92e47d6a03a2ef
Jody Hoon-Starr <jodyhoonstarr@gmail.com>
2019-12-16 16:14:23 -0500

    add dropdowns to age view

--- src/views/Age.vue
+++ src/views/Age.vue
@@ -1,14 +1,38 @@
 <template>
   <div>
     <h1 class="text-center">This is a the Age page</h1>
+    <SelectBar>
+      <v-col cols="12" xs="12" sm="9">
+        <DropDown
+          label="Age Groups"
+          hint="Range"
+          :items="ageGroups"
+          v-model="selectedAgeGroup"
+          multiple
+          close
+        ></DropDown>
+      </v-col>
+      <v-col cols="12" xs="12" sm="3">
+        <DropDown label="Cohort" hint="Year of discharge" :items="cohorts" v-model="selectedCohort"></DropDown>
+      </v-col>
+    </SelectBar>
   </div>
 </template>
 
 <script>
+import SelectBar from "@/components/SelectBar.vue";
+import DropDown from "@/components/DropDown.vue";
+
 export default {
   name: "Age",
+  components: {
+    SelectBar,
+    DropDown
+  },
   data() {
     return {
+      selectedAgeGroup: null,
+      selectedCohort: null,
       ageGroups: [{ id: "E01", label: "<25" }, { id: "E02", label: "25+" }],
       cohorts: [
         { id: "2000", label: "2000-2001" },
