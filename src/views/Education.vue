9028171add81b65cc9256801b6b84acde249201b
Jody Hoon-Starr <jodyhoonstarr@gmail.com>
2019-12-16 16:17:51 -0500

    add dropdowns to education view

--- src/views/Education.vue
+++ src/views/Education.vue
@@ -1,15 +1,39 @@
 <template>
   <div>
     <h1 class="text-center">This is the Education page</h1>
+    <SelectBar>
+      <v-col cols="12" xs="12" sm="9">
+        <DropDown
+          label="Education"
+          hint="Select Education Level"
+          :items="educationLevel"
+          v-model="selectedEducationLevel"
+          multiple
+          close
+        ></DropDown>
+      </v-col>
+      <v-col cols="12" xs="12" sm="3">
+        <DropDown label="Cohort" hint="Year of discharge" :items="cohorts" v-model="selectedCohort"></DropDown>
+      </v-col>
+    </SelectBar>
   </div>
 </template>
 
 <script>
+import SelectBar from "@/components/SelectBar.vue";
+import DropDown from "@/components/DropDown.vue";
+
 export default {
   name: "Education",
+  components: {
+    SelectBar,
+    DropDown
+  },
   data() {
     return {
-      education: [
+      selectedEducationLevel: null,
+      selectedCohort: null,
+      educationLevel: [
         { id: "F1", label: "General educational development (GED) test" },
         { id: "F2", label: "High school graduate" },
         { id: "F3", label: "Some college or higher" }
