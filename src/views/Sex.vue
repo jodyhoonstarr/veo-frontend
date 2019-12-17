8399b3df0693cbf35eacdd0c353e38612fa80e1c
Jody Hoon-Starr <jodyhoonstarr@gmail.com>
2019-12-17 10:49:05 -0500

    layout sex dropdowns

--- src/views/Sex.vue
+++ src/views/Sex.vue
@@ -1,15 +1,47 @@
 <template>
   <div>
     <h1 class="text-center">This is the Sex page</h1>
+    <SelectBar>
+      <v-col cols="12" xs="12" sm="9">
+        <DropDown
+          label="Sex"
+          hint="Sex Category"
+          :items="sexes"
+          v-model="selectedSex"
+          multiple
+          close
+        ></DropDown>
+      </v-col>
+      <v-col cols="12" xs="12" sm="3">
+        <DropDown
+          label="Cohort"
+          hint="Year of discharge"
+          :items="cohorts"
+          v-model="selectedCohort"
+        ></DropDown>
+      </v-col>
+    </SelectBar>
   </div>
 </template>
 
 <script>
+import SelectBar from "@/components/SelectBar.vue";
+import DropDown from "@/components/DropDown.vue";
+
 export default {
   name: "Sex",
+  components: {
+    SelectBar,
+    DropDown
+  },
   data() {
     return {
-      sexCategories: [{ id: "1", label: "Male" }, { id: "2", label: "Female" }],
+      selectedSex: null,
+      selectedCohort: null,
+      sexes: [
+        { id: "1", label: "Male" },
+        { id: "2", label: "Female" }
+      ],
       cohorts: [
         { id: "2000", label: "2000-2001" },
         { id: "2002", label: "2002-2003" },
