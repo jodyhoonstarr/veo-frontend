5f7e5eceb0db92964766f59e31173293058fb743
Jody Hoon-Starr <jodyhoonstarr@gmail.com>
2019-12-31 14:17:42 -0500

    create filter component

--- src/components/ChartCard.vue
+++ src/components/ChartCard.vue
@@ -7,16 +7,7 @@
             <v-card-title>{{ value.label }}</v-card-title>
           </v-col>
           <v-col xs="12" sm="6" class="text-right">
-            <v-btn-toggle v-model="activeFilter" mandatory tile group dense>
-              <template v-for="filter in filters" :value="filter">
-                <v-tooltip bottom>
-                  <template v-slot:activator="{ on }">
-                    <v-btn v-on="on">{{filter.short}}</v-btn>
-                  </template>
-                  <span>{{filter.label}}</span>
-                </v-tooltip>
-              </template>
-            </v-btn-toggle>
+            <ChartFilters @change="updateFilter" :value="filters"></ChartFilters>
           </v-col>
         </v-row>
         <BarChart :id="value.id" :svgwidth="width"></BarChart>
@@ -28,20 +19,24 @@
 
 <script>
 import BarChart from "@/components/BarChart.vue";
+import ChartFilters from "@/components/ChartFilters.vue";
 
 export default {
   name: "ChartCard",
-  components: { BarChart },
+  components: { BarChart, ChartFilters },
   props: ["value", "chartData", "filters"],
   data() {
     return {
       width: 0,
-      activeFilter: null
+      filter: null
     };
   },
   methods: {
     handleResize: function() {
       this.width = Math.floor(this.$refs[this.value.id].$el.clientWidth);
+    },
+    updateFilter(filter) {
+      this.filter = filter;
     }
   },
   mounted() {
---
+++ src/components/ChartFilters.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-btn-toggle @change="inputChange" mandatory tile group dense>
+    <template v-for="filter in value">
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on }">
+          <v-btn v-on="on" :value="filter">{{filter.short}}</v-btn>
+        </template>
+        <span>{{filter.label}}</span>
+      </v-tooltip>
+    </template>
+  </v-btn-toggle>
+</template>
+
+<script>
+export default {
+  name: "ChartFilters",
+  props: ["value"],
+  methods: {
+    inputChange(event) {
+      this.$emit("change", event);
+    }
+  }
+};
+</script>
