18d481197b1387991301fe2cf9ce47b086ce327e
Jody Hoon-Starr <jodyhoonstarr@gmail.com>
2020-01-08 12:29:47 -0500

    add defaults to filters and workaround change event not triggering on prop change in chartfilters

--- src/components/ChartFilters.vue
+++ src/components/ChartFilters.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-btn-toggle @change="inputChange" mandatory tile group dense>
+  <v-btn-toggle @change="inputChange" v-model="selected" mandatory tile group dense>
     <template v-for="filter in filters">
       <v-tooltip bottom>
         <template v-slot:activator="{ on }">
@@ -15,10 +15,26 @@
 export default {
   name: "ChartFilters",
   props: ["filters"],
+  data() {
+    return {
+      selected: null
+    };
+  },
   methods: {
     inputChange(event) {
       this.$emit("change", event);
     }
+  },
+  watch: {
+    filters: function() {
+      this.selected = this.filters.find(obj => obj.default === true);
+    },
+    selected: function() {
+      this.$emit("change", this.selected);
+    }
+  },
+  mounted() {
+    this.selected = this.filters.find(obj => obj.default === true);
   }
 };
 </script>
--- src/constants/filters.js
+++ src/constants/filters.js
@@ -5,27 +5,65 @@ export const BARCHARTFILTERS = [
     short: "Earnings",
     filters: [
       {
-        id: "year",
-        label: "Year",
-        short: "Year",
-        filters: [
-          { id: "y1", label: "One Year After Discharge", short: "One" },
-          { id: "y5", label: "Five Years After Discharge", short: "Five" },
-          { id: "y10", label: "Ten Years After Discharge", short: "Ten" }
-        ]
-      },
-      {
         id: "percentile",
         label: "Percentile",
         short: "Percentile",
         filters: [
-          { id: "p25", label: "Twenty-Fifth Percentile", short: "25%" },
-          { id: "p50", label: "Fiftieth Percentile  ", short: "50%" },
-          { id: "p75", label: "Seventy-Fifth Percentile", short: "75%" }
-        ]
+          {
+            id: "p25",
+            label: "Twenty-Fifth Percentile",
+            short: "25%",
+            default: false
+          },
+          {
+            id: "p50",
+            label: "Fiftieth Percentile  ",
+            short: "50%",
+            default: true
+          },
+          {
+            id: "p75",
+            label: "Seventy-Fifth Percentile",
+            short: "75%",
+            default: false
+          }
+        ],
+        default: true
+      },
+      {
+        id: "year",
+        label: "Year",
+        short: "Year",
+        filters: [
+          {
+            id: "y1",
+            label: "One Year After Discharge",
+            short: "One",
+            default: false
+          },
+          {
+            id: "y5",
+            label: "Five Years After Discharge",
+            short: "Five",
+            default: true
+          },
+          {
+            id: "y10",
+            label: "Ten Years After Discharge",
+            short: "Ten",
+            default: false
+          }
+        ],
+        default: false
       }
-    ]
+    ],
+    default: true
   },
-  { id: "emp", label: "Employment Status Count", short: "Emp" },
-  { id: "nonemp", label: "Nonemployment Status Count", short: "Nonemp" }
+  { id: "emp", label: "Employment Status Count", short: "Emp", default: false },
+  {
+    id: "nonemp",
+    label: "Nonemployment Status Count",
+    short: "Nonemp",
+    default: false
+  }
 ];
