dcff8f81a14bc3d630f16f17d512cb17a8185d7f
Jody Hoon-Starr <jodyhoonstarr@gmail.com>
2020-02-03 11:32:19 -0500

    choose interesting default filters

--- src/components/FiltersBar.vue
+++ src/components/FiltersBar.vue
@@ -7,17 +7,19 @@
           :id="constantFilters.id"
           :filters="constantFilters.filters"
           :label="constantFilters.label"
+          :value="dataType"
         ></ChartFilters>
       </v-col>
 
       <template v-if="dataType && dataType.hasOwnProperty('filters')">
-        <template v-for="(filter, idx) in dataType.filters">
+        <template v-for="filter in dataType.filters">
           <v-col cols="12" xs="12" sm="4">
             <ChartFilters
               :id="filter.id"
               :filters="filter.filters"
               :label="filter.label"
               :multiple="true"
+              :value="dataFilters[filter.id]"
               @change="handleFilter"
             >
             </ChartFilters>
@@ -43,26 +45,64 @@ export default {
         year: null,
         percentile: null
       },
+      testProp: null,
       allCategory: null
     };
   },
   methods: {
-    emit_event: function() {
+    emitEvent: function() {
       this.$emit("change", { type: this.dataType, filters: this.dataFilters });
     },
     handleDataTypeFilter: function(f) {
       this.dataType = f.selected;
-      this.dataFilters.percentile = null;
-      this.dataFilters.year = null;
+      if (this.dataType.id === "earnings") {
+        this.setAllPercentiles(); // toggle all percentiles
+        this.dataFilters.year = null; // allow component to select default year
+      } else {
+        this.dataFilters.percentile = null;
+        this.setAllYears();
+      }
       this.allCategory = null;
+    },
+    setAllPercentiles: function() {
+      this.dataFilters.percentile = this.constantFilters.filters[0].filters[0].filters;
+    },
+    setAllYears: function() {
+      this.dataFilters.year = this.constantFilters.filters[0].filters[1].filters;
     },
     handleFilter: function(f) {
       if (Array.isArray(f.selected)) {
         this.allCategory = f.id;
       }
       this.dataFilters[f.id] = f.selected;
-      this.emit_event();
+
+      // if this change is an array, null out the other filters
+      // so that their default values can be chosen
+      let changeFlag = false;
+      Object.keys(this.dataFilters).forEach(key => {
+        const prop = this.dataFilters[key];
+        if (
+          Array.isArray(prop) &&
+          Array.isArray(f.selected) &&
+          prop.length > 1 &&
+          f.selected.length > 1 &&
+          f.id !== key
+        ) {
+          this.dataFilters[key] = null;
+        }
+      });
+
+      // if the other events weren't nulled then emit event
+      // otherwise setting the null events above should trigger
+      // an event that bubbles back to this function
+      if (!changeFlag) {
+        this.emitEvent();
+      }
     }
+  },
+  mounted() {
+    this.dataType = this.constantFilters.filters[0];
+    this.setAllPercentiles();
   }
 };
 </script>
--- src/views/occupation/State.vue
+++ src/views/occupation/State.vue
@@ -3,7 +3,14 @@
     <h1 class="text-center">This is the Occupation by State page</h1>
     <SelectBar>
       <v-col cols="12" xs="12" sm="4">
-        <DropDown label="State" :items="states" v-model="selectedState" multiple close clearable></DropDown>
+        <DropDown
+          label="State"
+          :items="states"
+          v-model="selectedState"
+          multiple
+          close
+          clearable
+        ></DropDown>
       </v-col>
       <v-col cols="12" xs="12" sm="5">
         <DropDown
@@ -16,7 +23,11 @@
         ></DropDown>
       </v-col>
       <v-col cols="12" xs="12" sm="3">
-        <DropDown label="Cohort" :items="cohorts" v-model="selectedCohort"></DropDown>
+        <DropDown
+          label="Cohort"
+          :items="cohorts"
+          v-model="selectedCohort"
+        ></DropDown>
       </v-col>
     </SelectBar>
     <ChartArea>
