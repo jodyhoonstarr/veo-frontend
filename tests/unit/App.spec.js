1f5e1ae6242957dac84fc3a36c1c68b6ece6e587
Jody Hoon-Starr <jodyhoonstarr@gmail.com>
2020-02-14 22:09:13 -0500

    add app and header unit tests

--- tests/unit/App.spec.js
+++ tests/unit/App.spec.js
@@ -17,7 +17,27 @@ describe("App.vue", () => {
     wrapper = shallowMount(App, { localVue });
   });
 
+  afterEach(() => {
+    wrapper.destroy();
+  });
+
   it("renders a vue instance", () => {
     expect(wrapper.isVueInstance()).toBe(true);
+  });
+
+  it("contains the header", () => {
+    expect(wrapper.contains("header-stub")).toBe(true);
+  });
+
+  it("contains the footer", () => {
+    expect(wrapper.contains("footer-stub")).toBe(true);
+  });
+
+  it("contains the navbar", () => {
+    expect(wrapper.contains("navbar-stub")).toBe(true);
+  });
+
+  it("contains the router-view", () => {
+    expect(wrapper.contains("router-view-stub")).toBe(true);
   });
 });
---
+++ tests/unit/components/Header.spec.js
@@ -0,0 +1,42 @@
+//setup
+import "../../setup.js";
+
+// Libraries
+
+// Components
+import Header from "@/components/Header";
+
+// Utilities
+import { shallowMount, createLocalVue } from "@vue/test-utils";
+
+describe("Header.vue", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    const localVue = createLocalVue();
+    wrapper = shallowMount(Header, { localVue });
+  });
+
+  afterEach(() => {
+    wrapper.destroy();
+  });
+
+  it("renders a vue instance", () => {
+    expect(wrapper.isVueInstance()).toBe(true);
+  });
+
+  it("should have title text on sm+", () => {
+    const title = wrapper.find("v-toolbar-title-stub.hidden-xs-only");
+    expect(title.text()).toBe("Veterans Employment Outcomes");
+  });
+
+  it("should have title abbrev on xs", () => {
+    const title = wrapper.find("v-toolbar-title-stub.hidden-sm-and-up");
+    expect(title.text()).toBe("VEO");
+  });
+
+  it("should have lehd button on sm+", () => {
+    const text = wrapper.find("v-btn-stub > span.hidden-xs-only");
+    expect(text.text()).toBe("LEHD Home");
+  });
+});
