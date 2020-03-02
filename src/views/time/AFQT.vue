84e8f20bf3eece38a3378659bbafe20b0f56e3bb
Jody Hoon-Starr <jodyhoonstarr@gmail.com>
2020-03-02 17:02:35 -0500

    Stub in latest barchart selection dropdowns inplace on line chart

--- src/views/time/AFQT.vue
+++ src/views/time/AFQT.vue
@@ -1,58 +1,59 @@
 <template>
-  <div class="text-center">
-    <h1>AFQT</h1>
-    <h3 class="font-weight-light">Armed Forces Qualification Test</h3>
-    <select-bar>
-      <v-col cols="12" xs="12" sm="9">
-        <DropDown
-          label="AFQT"
-          :items="AFQTScores"
-          v-model="selectedAFQTScores"
-          multiple
-          clearable
-        ></DropDown>
+  <div>
+    <SelectBar>
+      <v-col cols="12" xs="12" sm="8" class="pb-0">
+        <GetData url="/metadata/label_afqtgrp.json">
+          <DropDownwRadio
+            slot-scope="{ response, loading }"
+            :loading="loading"
+            label="Occupation"
+            :items="response"
+            propname="labels"
+            id="occupation"
+            :toggle="activeToggle === 'afqt'"
+            @change="handleDropDownToggle"
+          ></DropDownwRadio>
+        </GetData>
       </v-col>
-      <v-col cols="12" xs="12" sm="3">
-        <DropDown
-          label="Cohort"
-          :items="cohorts"
-          v-model="selectedCohort"
-        ></DropDown>
+      <v-col cols="12" xs="12" sm="4" class="pb-0">
+        <GetData url="/metadata/label_2year_cohorts.json">
+          <DropDownwRadio
+            slot-scope="{ response, loading }"
+            :loading="loading"
+            label="Cohort"
+            id="cohort"
+            :items="response"
+            propname="labels"
+            :toggle="activeToggle === 'cohort'"
+            @change="handleDropDownToggle"
+          ></DropDownwRadio>
+        </GetData>
       </v-col>
-    </select-bar>
+    </SelectBar>
   </div>
 </template>
 
 <script>
 import SelectBar from "@/components/SelectBar.vue";
-import DropDown from "@/components/DropDown.vue";
+import DropDownwRadio from "@/components/DropDownwRadio";
+import GetData from "@/components/GetData";
 
 export default {
   name: "AFQT",
   components: {
     SelectBar,
-    DropDown
+    DropDownwRadio,
+    GetData
   },
   data() {
     return {
-      selectedAFQTScores: [],
-      selectedCohort: [],
-      cohorts: [
-        { id: "2000", label: "2000-2001" },
-        { id: "2002", label: "2002-2003" },
-        { id: "2004", label: "2004-2005" },
-        { id: "2006", label: "2006-2007" },
-        { id: "2008", label: "2008-2009" },
-        { id: "2010", label: "2010-2011" },
-        { id: "2012", label: "2012-2013" },
-        { id: "2014", label: "2014-2015" }
-      ],
-      AFQTScores: [
-        { id: "A1", label: "0-33" },
-        { id: "A2", label: "34-66" },
-        { id: "A3", label: "67-100" }
-      ]
+      activeToggle: "afqt"
     };
+  },
+  methods: {
+    handleDropDownToggle: function(f) {
+      console.log(f);
+    }
   }
 };
 </script>
