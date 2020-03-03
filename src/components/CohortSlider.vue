bdcd747bdfd12086528da3542f3727b65684aeed
Jody Hoon-Starr <jodyhoonstarr@gmail.com>
2020-03-03 15:00:39 -0500

    Style: format slider component style and spacing

---
+++ src/components/CohortSlider.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-row class="pt-5">
+    <v-col class="pt-5">
+      <v-range-slider
+        :tick-labels="seasons"
+        :value="[0, 1]"
+        min="0"
+        max="3"
+        ticks="always"
+        tick-size="4"
+        thumb-label="always"
+        color="primary"
+        thumb-color="primary"
+        track-color="grey"
+        track-fill-color="primary"
+      >
+        <template v-slot:thumb-label="props">
+          <v-icon dark>
+            {{ season(props.value) }}
+          </v-icon>
+        </template>
+      </v-range-slider>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "CohortSlider",
+  data() {
+    return {
+      seasons: ["Winter", "Spring", "Summer", "Fall"],
+      icons: ["mdi-snowflake", "mdi-leaf", "mdi-fire", "mdi-water"]
+    };
+  },
+  methods: {
+    season(val) {
+      return this.icons[val];
+    }
+  }
+};
+</script>
+
+<style scoped></style>
--- src/views/time/AFQT.vue
+++ src/views/time/AFQT.vue
@@ -1,61 +1,41 @@
 <template>
-  <div>
-    <SelectBar>
-      <v-col cols="12" xs="12" sm="6" class="pb-0">
-        <GetData url="/metadata/label_afqtgrp.json">
-          <DropDownNoRadio
-            slot-scope="{ response, loading }"
-            :loading="loading"
-            label="AFQT"
-            :items="response"
-            propname="labels"
-            id="afqt"
-          ></DropDownNoRadio>
-        </GetData>
-      </v-col>
-      <v-col cols="12" xs="12" sm="6" class="pb-0">
-        <v-range-slider
-          :tick-labels="seasons"
-          :value="[0, 1]"
-          min="0"
-          max="3"
-          ticks="always"
-          tick-size="4"
-        >
-          <template v-slot:thumb-label="props">
-            <v-icon dark>
-              {{ season(props.value) }}
-            </v-icon>
-          </template>
-        </v-range-slider>
-      </v-col>
-    </SelectBar>
-  </div>
+  <SelectBar>
+    <v-col cols="12" xs="12" sm="4" class="pb-0">
+      <GetData url="/metadata/label_afqtgrp.json">
+        <DropDownNoRadio
+          slot-scope="{ response, loading }"
+          :loading="loading"
+          label="AFQT"
+          :items="response"
+          propname="labels"
+          id="afqt"
+        ></DropDownNoRadio>
+      </GetData>
+    </v-col>
+    <v-col cols="12" xs="12" sm="8" class="pb-0">
+      <cohort-slider></cohort-slider>
+    </v-col>
+  </SelectBar>
 </template>
 
 <script>
 import SelectBar from "@/components/SelectBar.vue";
 import GetData from "@/components/GetData";
 import DropDownNoRadio from "@/components/DropDownNoRadio";
+import CohortSlider from "@/components/CohortSlider";
 
 export default {
   name: "AFQT",
   components: {
     DropDownNoRadio,
     SelectBar,
-    GetData
+    GetData,
+    CohortSlider
   },
   data() {
     return {
-      selected: null,
-      seasons: ["Winter", "Spring", "Summer", "Fall"],
-      icons: ["mdi-snowflake", "mdi-leaf", "mdi-fire", "mdi-water"]
+      selected: null
     };
-  },
-  methods: {
-    season(val) {
-      return this.icons[val];
-    }
   }
 };
 </script>
