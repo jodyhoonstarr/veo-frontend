3c7671b7084c2fbb8c25b9e89415c31988966097
Jody Hoon-Starr <jodyhoonstarr@gmail.com>
2020-03-10 11:27:42 -0400

    Bug: fix chart title text with new filter format

--- src/components/ChartCard.vue
+++ src/components/ChartCard.vue
@@ -2,22 +2,16 @@
   <v-card :loading="loading">
     <v-system-bar color="primary">
       <span v-if="loading" class="white--text">Loading...</span>
-      <span v-else class="white--text">TODO fill out filter text</span>
-      <!--      <span-->
-      <!--        v-else-if="-->
-      <!--          filters && activeToggle && filters.type && filters.type.length === 1-->
-      <!--        "-->
-      <!--        class="white&#45;&#45;text"-->
-      <!--        >{{ filters.type[0].label }} by {{ byString }}</span-->
-      <!--      >-->
-      <!--      <span-->
-      <!--        v-else-if="-->
-      <!--          filters && activeToggle && filters.type && filters.type.length === 2-->
-      <!--        "-->
-      <!--        class="white&#45;&#45;text"-->
-      <!--        >Count of {{ filters.type[0].short }} vs-->
-      <!--        {{ filters.type[1].short }} Veterans by {{ byString }}</span-->
-      <!--      >-->
+      <span
+        v-else-if="
+          filters &&
+            activeToggle &&
+            Array.isArray(filters.primary) &&
+            filters.primary.length === 1
+        "
+        class="white--text"
+        >{{ filters.primary[0].label }} by {{ byString }}</span
+      >
       <v-spacer></v-spacer>
     </v-system-bar>
     <slot></slot>
@@ -39,22 +33,13 @@ export default {
     activeToggle: {
       type: String,
       default: null
-    },
-    additionalString: {
-      type: String,
-      default: null,
-      required: false
     }
   },
   computed: {
     byString: function() {
-      const byStr =
-        this.activeToggle.charAt(0).toUpperCase() + this.activeToggle.slice(1);
-      if (this.additionalString != null) {
-        return `${byStr} ${this.additionalString}`;
-      } else {
-        return byStr;
-      }
+      return (
+        this.activeToggle.charAt(0).toUpperCase() + this.activeToggle.slice(1)
+      );
     }
   }
 };
