d4e716034ad378623c395729ffd88d0bdac4408e
Jody Hoon-Starr <jodyhoonstarr@gmail.com>
2020-04-02 16:04:29 -0400

    Improvement: set default route to default occ page

--- src/router/index.js
+++ src/router/index.js
@@ -7,8 +7,7 @@ Vue.use(VueRouter);
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: () => import(/* webpackChunkName: "home" */ "@/views/Home.vue")
+    redirect: "/occupation/detail/"
   },
   {
     path: "/occupation/",
@@ -136,17 +135,8 @@ const routes = [
           import(/* webpackChunkName: "curated" */ "@/views/custom/Curated.vue")
       }
     ]
-  }
-
-  // {
-  //   path: "/about",
-  //   name: "about",
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () =>
-  //     import(/* webpackChunkName: "about" */ "../views/About.vue")
-  // }
+  },
+  { path: "*", redirect: "/" }
 ];
 
 const router = new VueRouter({
