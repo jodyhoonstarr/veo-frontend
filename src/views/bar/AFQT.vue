829f59cf9bf4c6743a4abb41335bc5a1e5031345
Jody Hoon-Starr <jodyhoonstarr@gmail.com>
2020-11-13 15:03:05 -0500

    Feature: add afqt x time bar view

---
+++ src/views/bar/AFQT.vue
@@ -0,0 +1,192 @@
+<template>
+  <div>
+    <SelectBar>
+      <v-col cols="12" xs="12" sm="6" class="pb-0">
+        <GetData :url="dataPath('metadata/label_industry.json')">
+          <DropDownwRadio
+            id="industry"
+            v-model="industryObj"
+            slot-scope="{ response, loading }"
+            :loading="loading"
+            label="Industry"
+            :items="response"
+            propname="labels"
+          ></DropDownwRadio>
+        </GetData>
+      </v-col>
+      <v-col cols="12" xs="12" sm="6" class="pb-0">
+        <GetData :url="dataPath('metadata/label_2year_cohorts.json')">
+          <DropDownwRadio
+            id="cohort"
+            v-model="cohortObj"
+            slot-scope="{ response, loading }"
+            :loading="loading"
+            label="Cohort"
+            :items="response"
+            propname="labels"
+          ></DropDownwRadio>
+        </GetData>
+      </v-col>
+    </SelectBar>
+    <GetData
+      :url="dataPath('data/veons.csv')"
+      :emit="true"
+      @change="({ response }) => (this.csvData = response)"
+    >
+      <ChartCard
+        slot-scope="{ loading }"
+        :loading="loading"
+        :filters="filters"
+        :active-toggle="activeToggle"
+      >
+        <FiltersBar chart-type="bar" @change="handleFilters"></FiltersBar>
+        <Chart
+          :chart-type="chartType"
+          :loading="loading"
+          :chart-data="chartData"
+          :chart-colors="chartColors"
+          :chart-data-type="chartDataType"
+          chart-label="Exit Cohort"
+          :chart-data-zip="dataPath('downloads/VEO-State.zip')"
+        ></Chart>
+      </ChartCard>
+    </GetData>
+  </div>
+</template>
+
+<script>
+import SelectBar from "@/components/SelectBar.vue";
+import GetData from "@/components/GetData";
+import DropDownwRadio from "@/components/DropDownwRadio";
+import ChartCard from "@/components/ChartCard";
+import FiltersBar from "@/components/FiltersBar";
+import Chart from "@/components/Chart";
+import { GROUPCOLUMN } from "@/constants/lookups";
+import {
+  createChartData,
+  filterRows,
+  getChartDataType,
+  joinPublicPath,
+  simplifiyRows
+} from "@/lib/utils";
+import { filterSelect } from "@/lib/filterselect";
+
+export default {
+  name: "Industry",
+  components: {
+    DropDownwRadio,
+    SelectBar,
+    GetData,
+    ChartCard,
+    FiltersBar,
+    Chart
+  },
+  data() {
+    return {
+      name: "industry",
+      chartType: "bar",
+      csvData: null,
+      industryObj: {
+        selected: [
+          { id: "00", label: "All NAICS sectors" },
+          {
+            id: "11",
+            label: "Agriculture, Forestry, Fishing and Hunting"
+          },
+          { id: "21", label: "Mining, Quarrying, and Oil and Gas Extraction" },
+          { id: "22", label: "Utilities" }
+        ],
+        toggle: true
+      },
+
+      cohortObj: { selected: null, toggle: false },
+      initialFilters: {
+        primary: filterSelect("earnings", "primary"),
+        secondary: filterSelect("earnings", "secondary", "p50"),
+        tertiary: filterSelect("earinings", "tertiary", "all")
+      },
+      filters: {
+        colors: null,
+        filters: null,
+        type: null
+      },
+      activeToggle: "industry"
+    };
+  },
+  computed: {
+    industry: function() {
+      return this.industryObj.selected;
+    },
+    cohort: function() {
+      return this.cohortObj.selected;
+    },
+    dataSelections: function() {
+      return [
+        { data: this.cohort, prop: "cohort" },
+        { data: this.industry, prop: "industry" }
+      ];
+    },
+    chartDataType: function() {
+      return getChartDataType(this.filters);
+    },
+    activeToggleProp: function() {
+      return GROUPCOLUMN[this.activeToggle];
+    },
+    csvDataRows: function() {
+      return filterRows(this.csvData, this.dataSelections);
+    },
+    csvDataRowsSimple: function() {
+      return simplifiyRows(
+        this.csvDataRows,
+        this.filters,
+        this.activeToggleProp
+      );
+    },
+    chartData: function() {
+      return createChartData(
+        this.csvDataRowsSimple,
+        this.filters,
+        this.activeToggleProp,
+        this.dataSelections
+      );
+    },
+    chartColors: function() {
+      if (
+        this.filters != null &&
+        Object.prototype.hasOwnProperty.call(this.filters, "colors")
+      ) {
+        return this.filters.colors;
+      }
+      return null;
+    }
+  },
+  watch: {
+    industryObj: function() {
+      this.setActiveToggle(this.industryObj, "industry");
+    },
+    cohortObj: function() {
+      this.setActiveToggle(this.cohortObj, "cohort");
+    }
+  },
+  methods: {
+    dataPath: function(str) {
+      return joinPublicPath(str);
+    },
+    handleFilters: function(f) {
+      if (f == null) {
+        return null;
+      }
+      this.filters = f;
+    },
+    setActiveToggle: function(changedObj, changedStr) {
+      if (changedObj.toggle) {
+        this.activeToggle = changedStr;
+        // set all the other toggles to false if this one got switched on
+        [this.industryObj, this.cohortObj]
+          .filter(o => o !== changedObj)
+          .forEach(o => (o.toggle = false));
+      }
+    }
+  }
+};
+</script>
