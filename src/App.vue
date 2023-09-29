c56a7b851e3417fe379556c6126489e135e6c1dd
Jody Hoon-Starr <jodyhoonstarr@gmail.com>
2023-09-29 15:39:12 -0400

    feat: add shutdown banner

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <v-app>
     <v-main>
+      <ShutdownBanner />
       <Header />
       <Navbar />
       <router-view />
@@ -13,6 +14,7 @@
 import Header from "./components/Header.vue";
 import Footer from "./components/Footer.vue";
 import Navbar from "./components/Navbar.vue";
+import ShutdownBanner from "./components/ShutdownBanner.vue";
 
 export default {
   name: "App",
@@ -20,7 +22,8 @@ export default {
   components: {
     Header,
     Footer,
-    Navbar
+    Navbar,
+    ShutdownBanner
   }
 };
 </script>
---
+++ src/components/ShutdownBanner.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-banner>
+    <span class="error--text">
+      Due to the lapse of federal funding, this website is not being updated. Any inquiries submitted via <a
+        href="https://www.census.gov"
+        target="_blank"
+      >www.census.gov</a> will not be answered until appropriations are enacted.
+    </span>
+  </v-banner>
+</template>
